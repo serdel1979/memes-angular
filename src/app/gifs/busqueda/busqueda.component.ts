d0d6634a1db23cbe4d35f726dfd8d89dadad82ee
Sergio De Luca <sdlbsso@gmail.com>
2022-08-13 17:05:22 -0300

    haciendo buscar

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AbstractType, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  buscar(){
+      console.log(this.txtBuscar.nativeElement.value);
+      this.txtBuscar.nativeElement.value = "";
   }
 
 }
